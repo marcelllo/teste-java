04aba60772e587affc3f2cd202a35f1a24e03d89
Marcelo Santos <marcello.santos@gmail.com>
2024-11-28 19:36:15 -0300

    Inclusão do triplo do número

--- src/Aula1.java
+++ src/Aula1.java
@@ -5,9 +5,10 @@ public class Aula1 {
         Scanner leia = new Scanner(System.in);
         System.out.println("Digite um número");
         int numero = leia.nextInt();
-
         int dobro = numero * 2;
+        int triplo = numero * 3;
 
         System.out.println("O dobro de " + numero + " é " + dobro);
+        System.out.println("O triplo de " + numero + " é " + triplo);
     }
 }
