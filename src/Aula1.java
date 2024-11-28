f25e4cb814c25befc4b52991b9e0171fcb839de4
Marcelo Santos <marcello.santos@gmail.com>
2024-11-28 19:33:49 -0300

    Criando projeto para netbeans

--- src/Aula1.java
+++ src/Aula1.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Aula1 {
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
-        System.out.println("Digite um nÃºmero");
+        System.out.println("Digite um número");
         int numero = leia.nextInt();
 
         int dobro = numero * 2;
 
-        System.out.println("O dobro de " + numero + " Ã© " + dobro);
+        System.out.println("O dobro de " + numero + " é " + dobro);
     }
 }
